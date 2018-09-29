cfe0bbd43332f83d6ccb38f2b38ec494887c6f58
Dror Harari <DrorHarari@users.noreply.github.com>
2018-09-29 04:09:22 +0300

    Fix build

--- etc/build.py
+++ etc/build.py
@@ -4,20 +4,22 @@ import os
 import zipfile
 
 PACKAGE_NAME="Cvt"
+FILES=["cvt.py","cvt.ini","cvt.ico","LICENSE","lib/safeeval.py","data/cvtdefs.json"]
 
 ETC_FOLDER=Path(__file__).parent
 PACKAGE_FOLDER=ETC_FOLDER.parent
 PACKAGE_FILE=Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
 
-zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+print(f"Creating package file {PACKAGE_FILE}")
 
-# File to include in the package
-os.chdir(PACKAGE_FOLDER)
-zf.write('cvt.py')
-zf.write('lib/safeeval.py')
-zf.write('data/cvtdefs.json')
-zf.write('cvt.py')
-zf.write('cvt.ini')
-zf.write('cvt.ico')
-zf.write('LICENSE')
-zf.close()
+try:
+    zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+    for f in FILES:
+        zf.write(Path.joinpath(Path('..'), f), f)
+
+    zf.close()
+except Exception as exc:
+    print(f"Failed to create package {PACKAGE_NAME}. {exc}")
+    os._exit(1)
+
+print(f"Done")
