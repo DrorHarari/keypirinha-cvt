9020b3646b1a9ef4894c3f048fd44c4015801f14
Dror Harari <DrorHarari@users.noreply.github.com>
2021-10-23 03:47:45 +0300

    Update build.py

--- etc/build.py
+++ etc/build.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import os
 import zipfile
 
-PACKAGE_NAME = "Cvt"
+PACKAGE_NAME = "cvt"
 PACKAGE_VERSION = f" (version {os.environ['PACKAGE_VERSION']})" \
     if "PACKAGE_VERSION" in os.environ else ""
 FILES = ["cvt.py", "cvt.ini", "cvt.ico", "LICENSE", "lib/safeeval.py", "data/cvtdefs.json"]
 
+# Following is common code
 ETC_FOLDER = Path(__file__).parent
 PACKAGE_FOLDER = ETC_FOLDER.parent
 PACKAGE_FILE = Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
@@ -17,11 +18,11 @@ print(f"Creating package file {PACKAGE_FILE}")
 try:
     zf = zipfile.ZipFile(PACKAGE_FILE, 'w', zipfile.ZIP_DEFLATED)
     for f in FILES:
-        if f == "cvt.ini" and PACKAGE_VERSION:  # Replace {version} in cvt.ini
-            with open(Path.joinpath(Path('..'), f), "r", encoding="utf-8") as cvtinif:
-                cvtini = ''.join(cvtinif.readlines())
-                cvtini = cvtini.replace("{version}", PACKAGE_VERSION)
-                zf.writestr(f, cvtini.encode())
+        if PACKAGE_VERSION and f == f"{PACKAGE_NAME}.ini":  # Replace {version} in package configuration template
+            with open(Path.joinpath(Path('..'), f), "r", encoding="utf-8") as inif:
+                ini = ''.join(inif.readlines())
+                ini = cvtini.replace("{#version#}", PACKAGE_VERSION)
+                zf.writestr(f, ini.encode())
             continue
 
         zf.write(Path.joinpath(Path('..'), f), f)
